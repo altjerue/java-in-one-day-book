59b096a3e42f4ac843d8be12cb0739da1d0bb618
Jesús Rueda-Becerril <jm.ruebe@gmail.com>
2022-11-06 09:19:19 -0800

    Hello World project

---
+++ helloworld/src/main/java/org/jmrb/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.jmrb.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int myLength = "Hello World".length();
+        System.out.println(myLength);
+        System.out.println("Hello world!");
+    }
+}
