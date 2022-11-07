607154f728203217f97f93f280af6980b4418715
Jesús Rueda-Becerril <jm.ruebe@gmail.com>
2022-11-06 16:51:02 -0800

    Creating ErrorDemo2

---
+++ ErrorDemo2/src/main/java/org/jmrb/errordemo2/ErrorDemo2.java
@@ -0,0 +1,24 @@
+package org.jmrb.errordemo2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ErrorDemo2 {
+    public static void main(String[] args) {
+        int choice;
+        Scanner input = new Scanner(System.in);
+        int[] numbers = {10, 11, 12, 13, 14, 15};
+        System.out.print("Please enter the index of the array: ");
+
+        try {
+            choice = input.nextInt();
+            System.out.printf("numbers[%d] = %d%n", choice, numbers[choice]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Index is invalid.");
+        } catch (InputMismatchException e) {
+            System.out.println("Error: You did not enter an integer.");
+        } catch (Exception e) {
+            System.out.printf(e.getMessage());
+        }
+    }
+}
