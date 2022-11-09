80e6b86efa5416476990d1a7e939674f87ccb27c
Jesús Rueda-Becerril <jm.ruebe@gmail.com>
2022-11-09 13:27:16 -0800

    Adding the passPrimitive, and passReference methods in MyClass class

--- ArrayMethodDemo/src/main/java/org/jmrb/arraymethoddemo/ArrayMethodDemo.java
+++ ArrayMethodDemo/src/main/java/org/jmrb/arraymethoddemo/ArrayMethodDemo.java
@@ -12,5 +12,19 @@ public class ArrayMethodDemo {
         int[] myArray2 = amd.returnArray();
         System.out.println(Arrays.toString(myArray2));
 
+        /*
+        Passing primitive type vs reference type as parameters of a method
+
+         */
+        int number = 2;
+        System.out.println("number before = " + number);
+        amd.passPrimitive(number);
+        System.out.println("number after = " + number);
+
+        System.out.print("\n");
+
+        System.out.println("MyArray[1] before = " + myArray[1]);
+        amd.passReference(myArray);
+        System.out.println("myArray[1] after = " + myArray[1]);
     }
 }
--- ArrayMethodDemo/src/main/java/org/jmrb/arraymethoddemo/MyClass.java
+++ ArrayMethodDemo/src/main/java/org/jmrb/arraymethoddemo/MyClass.java
@@ -12,4 +12,14 @@ class MyClass {
         }
         return a;
     }
+
+    public void passPrimitive(int primitivePara) {
+        primitivePara = 10;
+        System.out.println("Value inside method = " + primitivePara);
+    }
+
+    public void passReference(int[] refPara) {
+        refPara[1] = 5;
+        System.out.println("Value inside method = " + refPara[1]);
+    }
 }
