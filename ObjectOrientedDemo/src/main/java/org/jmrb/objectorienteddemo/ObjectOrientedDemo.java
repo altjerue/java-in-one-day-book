8ac34fc323c8c5a8b3a803ab95e6a741607c8d7e
Jesús Rueda-Becerril <jm.ruebe@gmail.com>
2022-11-08 16:39:32 -0800

    Adding constructors, getters, setters, and instantiating Staff objects

--- ObjectOrientedDemo/src/main/java/org/jmrb/objectorienteddemo/ObjectOrientedDemo.java
+++ ObjectOrientedDemo/src/main/java/org/jmrb/objectorienteddemo/ObjectOrientedDemo.java
@@ -2,6 +2,30 @@ package org.jmrb.objectorienteddemo;
 
 public class ObjectOrientedDemo {
     public static void main(String[] args) {
+        /*
+        Instantiating staff1 with one parameter constructor
+         */
+        Staff staff1 = new Staff("Peter");
+        /*
+        Using dot operator to access setHoursWorked public setter method
+         */
+        staff1.setHoursWorked(160);
+        /*
+        Instantiating pay with two parameter constructor
+         */
+        int pay = staff1.calculatePay(1000, 400);
+        System.out.println("Pay = " + pay);
 
+        // ----- Another example with setters -----
+        Staff staff2 = new Staff("Jane", "Lee");
+        staff2.setHoursWorked(160);
+        pay = staff1.calculatePay();
+        System.out.println("Pay = " + pay);
+
+        System.out.println("\n\nUpdating Jane's Hours Worked to -10");
+        staff2.setHoursWorked(-10);
+        System.out.println("\nHours Worked = " + staff2.getHoursWorked());
+        pay = staff2.calculatePay();
+        System.out.println("Pay = " + pay);
     }
 }
--- ObjectOrientedDemo/src/main/java/org/jmrb/objectorienteddemo/Staff.java
+++ ObjectOrientedDemo/src/main/java/org/jmrb/objectorienteddemo/Staff.java
@@ -3,15 +3,42 @@ package org.jmrb.objectorienteddemo;
 public class Staff {
     /*
     Fields of our class
+
+    General note: fields and variable are declared in a similar manner. Fields
+    are declared for a class while variables are declared in a method.
      */
     private String nameOfStaff;
     private final int hourlyRate = 30;
     private int hoursWorked;
 
+    /*
+    Constructor with one parameter
+     */
+    public Staff(String name) {
+        nameOfStaff = name;
+        System.out.println("\n" + nameOfStaff);
+        System.out.println("---------------------------");
+    }
+
+    /*
+    Constructor with two parameters
+     */
+    public Staff(String firstName, String lastName) {
+        nameOfStaff = firstName + " " + lastName;
+        System.out.println("\n" + nameOfStaff);
+        System.out.println("---------------------------");
+    }
+
+    /*
+    Methods of our class
+     */
     public void printMessage() {
         System.out.println("Calculating Pay...");
     }
 
+    /*
+    Method without parameters
+     */
     public int calculatePay() {
         printMessage();
 
@@ -23,5 +50,38 @@ public class Staff {
         } else {
             return -1;
         }
+    }
+
+    /*
+    Method with the same name as above, but with two parameters
+     */
+    public int calculatePay(int bonus, int allowance) {
+        printMessage();
+
+        if (hoursWorked > 0) {
+            return hoursWorked * hourlyRate + bonus + allowance;
+        } else {
+            return 0;
+        }
+
+    }
+
+    /*
+    Setter
+     */
+    public void setHoursWorked(int hours) {
+        if (hours > 0) {
+            hoursWorked = hours;
+        } else {
+            System.out.println("Error: HoursWorked Cannot be smaller than zero");
+            System.out.println("Error: HoursWorked is not updated");
+        }
+    }
+
+    /*
+    Getter
+     */
+    public int getHoursWorked() {
+        return hoursWorked;
     }
 }
